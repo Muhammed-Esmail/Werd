db44ed99214efe169ee152b75eee6424c3d47be5
Muhammed-Esmail <muhammedesmail101@gmail.com>
2026-02-04 19:24:43 +0200

    Werd Screen UI

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 
 const explore = () => {
   return (
-    <View>
-      <Text>explore</Text>
+    <View className="flex-1 bg-matteBlack">
     </View>
   )
 }
--- app/(tabs)/werd.tsx
+++ app/(tabs)/werd.tsx
@@ -1,7 +1,8 @@
-import { MaterialIcons } from '@expo/vector-icons'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { MaterialIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Card = ({sideIcon, sideText, mainText, underText}: any) =>{
   return  (
@@ -23,6 +24,72 @@ const Card = ({sideIcon, sideText, mainText, underText}: any) =>{
   )
 }
 
+const TodayCard = () => {
+  
+  const onReadTodaysWerd = () => {
+
+  }
+
+  const onCompleteManually = () => {
+    
+  }
+
+  return (
+    <View className='bg-white border-[2px] border-light-300 rounded-[20px]'>
+      {/* Surah Name + Pages + Icon */}
+      
+      <View className='flex-row justify-between mt-3 '>
+        {/* Surah Name + Pages */}
+        <View className='gap-2 mt-5 ml-5'>
+          <Text className='font-bold text-[25px]'>Al-Baqarah</Text>
+          <Text className='text-mutedWhite'>Pages 10-25</Text>
+        </View>
+        <View className='border-[2px] border-primaryGold rounded-[20px] bg-goldGlow p-4 justify-center items-center mr-5 mt-4' >
+          <MaterialIcons name='menu-book' size={35} color={'#D4AF37'}/>
+        </View>        
+      </View>
+      {/* Progress bar */}
+      
+      <View className='mt-6 ml-5 mr-5 mb-3'>
+        <View className='flex-row justify-between items-center'>
+          <Text className='text-xs font-bold text-white'>CURRENT PROGRESS</Text>
+          <Text className='text-primaryGold font-semibold'>6/15 pages</Text>
+        </View>     
+        <View className='bg-borderDark h-2.5 rounded-full overflow-hidden mt-2'>
+          <LinearGradient
+            // Colors from your palette: primaryGold to goldLight
+            colors={['#D4AF37', '#F1E5AC']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={{ width: '40%', height: '100%', borderRadius: 999 }}
+          />
+        </View>
+      </View>
+      {/* Read Button */}
+      <TouchableOpacity
+        onPress={onReadTodaysWerd}
+        activeOpacity={0.7}
+        className='mt-5 h-20 p-4 bg-goldLight rounded-2xl w-[80%] items-center justify-center self-center'
+      >
+        <View className='items-center justify-center'>
+          <Text className='font-bold tracking-wide text-[17px]'>Read Today's Werd</Text>
+        </View>
+      </TouchableOpacity>
+      {/* Manual Completion */}
+      <TouchableOpacity
+        onPress={onCompleteManually}
+        activeOpacity={0.7}
+        className='mt-5 h-15 p-4 bg-mutedWhite border-[1px] border-light-300 rounded-2xl w-[80%] items-center justify-center self-center mb-7'
+      >
+        <View className='items-center justify-center'>
+          <Text className='font-bold tracking-wide text-[15px]'>Mark as Completed Manually</Text>
+        </View>
+      </TouchableOpacity>
+
+    </View>
+  )
+}
+
 const werd = () => {
   return (
     <SafeAreaView className="flex-1 bg-matteBlack">
@@ -40,7 +107,20 @@ const werd = () => {
 
       </View>
 
-      {/* Today's */}
+      {/* Today's Goal */}
+      <View className='mt-10 ml-4 mr-4'>
+        <View className='flex-row justify-between mb-3'>
+          
+          <Text className='font-bold tracking-widest'>TODAY'S GOAL</Text>
+
+          <Text className='text-xs text-primaryGold font-bold'>WERD #04</Text>
+
+        </View>
+        <View>
+          {/* Main Card */}
+          <TodayCard />
+        </View>
+      </View>
 
     </SafeAreaView>
   )
