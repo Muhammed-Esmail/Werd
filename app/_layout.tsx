9b8a82cd8973fdafd5ffd1cd576ca4a546cd0da2
Ahmad Amin <68996193+Amadoo42@users.noreply.github.com>
2026-02-19 15:05:56 +0000

    Added OTA update capability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout() {
             try {
                 console.log("Initializing database...");
 
-                await DB.initDB(1);
+                await DB.initDB(0);
                 // await DB.initDB(1);
                 // await DB.test(114, 114);
 
